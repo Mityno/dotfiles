91ec77de4110b6b845f190fe3f397b6cbe3efeba
Mityno <pititcowboy@gmail.com>
2026-08-15 10:32:57 +0200

    feat: Add global gitignore file

--- scripts/make-symbolic-link.sh
+++ scripts/make-symbolic-link.sh
@@ -2,4 +2,4 @@
 ln -fs --backup ~/dotfiles/.zshrc ~/dotfiles/.bashrc ~/dotfiles/.bash_aliases ~/dotfiles/.bash_profile ~/dotfiles/.zprofile ~/dotfiles/.gitconfig ~
 
 # Other config (inside .config)
-ln -fs --backup ~/dotfiles/kitty ~/dotfiles/nvim ~/dotfiles/neovide ~/dotfiles/yazi ~/dotfiles/starship.toml ~/.config/
+ln -fs --backup ~/dotfiles/kitty ~/dotfiles/nvim ~/dotfiles/neovide ~/dotfiles/yazi ~/dotfiles/starship.toml ~/dotfiles/.global-gitignore ~/.config/
